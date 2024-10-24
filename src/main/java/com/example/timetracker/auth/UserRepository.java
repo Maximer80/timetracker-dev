042a41c5e18ad8b0c893f8848d2903095c6abdac
Maxim <inffomax@google.com>
2024-10-24 22:46:11 +0300

    Fix error authorization

--- src/main/java/com/example/timetracker/auth/UserRepository.java
+++ src/main/java/com/example/timetracker/auth/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example.timetracker.auth;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
