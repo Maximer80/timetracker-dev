2381e6b80a3c18c862e80ad8fd8d95d99fedd642
Maximer80 <inffomax@gmail.com>
2024-11-12 17:56:08 +0200

    Add scan @Ent@EntityScan fot Spring Boot

--- src/main/java/com/example/timetracker/TimetrackerApplication.java
+++ src/main/java/com/example/timetracker/TimetrackerApplication.java
@@ -2,8 +2,10 @@ package com.example.timetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.timetracker") // укажите путь к вашему основному пакету
 public class TimetrackerApplication {
 
 	public static void main(String[] args) {
