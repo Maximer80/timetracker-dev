270be52f1889cf609d50e0a85b9c07551f881abe
Maximer80 <inffomax@gmail.com>
2024-11-13 15:36:39 +0200

    Add Lombok in AuthController

--- src/main/java/com/example/timetracker/auth/AuthController.java
+++ src/main/java/com/example/timetracker/auth/AuthController.java
@@ -1,5 +1,7 @@
 package com.example.timetracker.auth;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -54,24 +56,9 @@ public class AuthController {
 }
 
 // Класс для передачи данных при логине
+@Getter
+@Setter
 class UserLoginRequest {
     private String username;
     private String password;
-
-    // Геттеры и сеттеры
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
